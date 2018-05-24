d959d1c5b5353ab3729fe79589bba0bc02bdcb1f
Edwin <edwin@kenzie.academy>
2018-05-24 11:28:39 -0400

    Finished scaffold for assessment.

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// This URL can be combined with an photo id to fetch an photo.
+const PHOTO_URL = "https://picsum.photos/200?photo=";
+// This URL can be used to get an array of objects that contain information
+// about various photos.
+const PHOTO_LIST_URL = "https://picsum.photos/list";
+
+class App extends Component {
+  // 1. Declare a state object that will be used to track an array of photos
+  state = {
+  }
+
+  // 2. Declare a life cycle method that:
+  //  - will be called after the component is initially rendered
+  // - will fetch an array of photos
+  // - will add that array of photos to state once received
+  render() {
+    const { photos = [] } = this.state;
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Photo Wall</h1>
+          <p>Start by reading App.jsx and completing the numbered steps. Afterward, delete this paragraph.</p>
+        </header>
+        <div className="collage">
+            {/* We use map here because Array.prototype.map is an expression,
+              * and for loops are not. You'll learn more about this soon! 
+              */}
+            {photos.map( photo => 
+                <img alt={/* 3. Fill me in with the photo's filename */ ""}
+                     key={/* 4. Fill me in with the photo's id */ ""}
+                     src={/* 5. Fill me in with the photo's URL */ ""}
+                />
+            )}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
