0bd0fba951cca9aac80a58ddd7dedc2d6cd8afc0
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-07-26 10:03:37 -0400

    prettier autoformatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 // This URL can be combined with an photo id to fetch an photo.
 const PHOTO_URL = "https://picsum.photos/200?photo=";
@@ -9,8 +9,7 @@ const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
   // 1. Declare a state object that will be used to track an array of photos
-  state = {
-  }
+  state = {};
 
   // 2. Declare a life cycle method
   // This life cycle method should:
@@ -25,20 +24,21 @@ class App extends Component {
           <h1>Photo Wall</h1>
           <p>
             Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and
-            complete the instructions there.
+            Afterward, delete this paragraph. Then, open up App.css and complete
+            the instructions there.
           </p>
         </header>
         <div className="collage">
-            {/* We use map here because Array.prototype.map is an expression,
-              * and for loops are not. You'll learn more about this soon! 
-              */}
-            {photos.map( photo => 
-                <img alt={/* 3. Fill me in with the photo's filename */ ""}
-                     key={/* 4. Fill me in with the photo's id */ ""}
-                     src={/* 5. Fill me in with the photo's URL */ ""}
-                />
-            )}
+          {/* We use map here because Array.prototype.map is an expression,
+           * and for loops are not. You'll learn more about this soon!
+           */}
+          {photos.map(photo => (
+            <img
+              alt={/* 3. Fill me in with the photo's filename */ ""}
+              key={/* 4. Fill me in with the photo's id */ ""}
+              src={/* 5. Fill me in with the photo's URL */ ""}
+            />
+          ))}
         </div>
       </React.Fragment>
     );
